efc587bab2cd231ce002acbcaf3224dc1b9408f7
Brian Sayler <sayler.b@gmail.com>
2016-12-30 21:06:14 -0500

    more stuff to App component

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './lib/App'
 
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
-const LitComponent = () => {
-  return (
-    <div>
-      <h1>Welcome to Lit Weather!</h1>
-      <h1>{String.fromCharCode(0xD83D, 0xDD25)}</h1>
-    </div>
-  )}
-
-render(<LitComponent />, document.getElementById('app'))
+render(<App />, document.getElementById('app'))
